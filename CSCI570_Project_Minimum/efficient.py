ceb10090421f86c717fbe94a520657324b7fec3c
許庭瑋 Hsu Ting Wei <hsutimwei@gmail.com>
2025-11-19 14:58:23 -0800

    1

--- CSCI570_Project_Minimum/efficient.py
+++ CSCI570_Project_Minimum/efficient.py
@@ -59,22 +59,22 @@ def efficient_sequence_alignment(X, Y, delta, alpha):
     """
     m, n = len(X), len(Y)
 
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    dp = [[0] * (n + 1) for _ in range(2)]
 
     # base cases
-    for i in range(m + 1):
-        dp[i][0] = i * delta
+    dp[0][0] = 0
 
     for j in range(n + 1):
         dp[0][j] = j * delta
 
     # dp table
     for i in range(1, m + 1):
+        dp[i % 2][0] = i * delta    
         for j in range(1, n + 1):
 
-            dp[i][j] = min(dp[i - 1][j - 1] + alpha[X[i - 1], Y[j - 1]],
-                           dp[i - 1][j] + delta,
-                           dp[i][j - 1] + delta)
+            dp[i % 2][j] = min(dp[(i - 1) % 2][j - 1] + alpha[X[i - 1], Y[j - 1]],
+                               dp[(i - 1) % 2][j] + delta,
+                               dp[i % 2][j - 1] + delta)
 
     minimum_alignment_cost = dp[m][n]
 
